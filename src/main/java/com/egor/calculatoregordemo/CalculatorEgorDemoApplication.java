8afde71d2b8e67b58026d6096b146a4dfcf71b35
Егор <azazaz.com534@gmail.com>
2023-04-17 16:48:54 +0300

    initial commit

---
+++ src/main/java/com/egor/calculatoregordemo/CalculatorEgorDemoApplication.java
@@ -0,0 +1,14 @@
+package com.egor.calculatoregordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorEgorDemoApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorEgorDemoApplication.class, args);
+	}
+
+}
